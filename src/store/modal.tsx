36a9836af1c203986ae10271132fb20a2a2448ab
6lueparr0t <6lueparr0t@gmail.com>
2024-06-27 02:42:36 +0900

    feat: portfolio start

---
+++ src/store/modal.tsx
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import type { Modal } from "@/components/components.d";
+
+type Store = {
+  modals: Modal[];
+  clearModals: () => void;
+  pushModals: (modal: Modal) => void;
+  popModals: () => void;
+};
+
+const modalStore = create<Store>()((set) => ({
+  modals: [],
+  clearModals: () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    set((_) => {
+      return { modals: [] };
+    });
+  },
+  pushModals: (modal: Modal) => {
+    set((state) => {
+      state.modals.push(modal);
+      return { modals: [...state.modals] };
+    });
+  },
+  popModals: () => {
+    set((state) => {
+      state.modals.pop();
+      return { modals: [...state.modals] };
+    });
+  },
+}));
+
+export default modalStore;
