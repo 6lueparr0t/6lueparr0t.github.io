bd4c21b7b92403da719fc5682f758995aa2e07b2
6lueparr0t <6lueparr0t@gmail.com>
2024-07-01 23:30:27 +0900

    feat: about 완료

--- src/components/About/Gameboy.tsx
+++ src/components/About/Gameboy.tsx
@@ -1,16 +1,32 @@
 import "@/style/gameboy.scss";
 import { useState, useEffect, useRef } from "react";
+import { ReactTyped } from "react-typed";
 import loading from "@/assets/loading.gif";
 import ash from "@/assets/ash.gif";
-
-// import { useState, useEffect } from "react";
-// import modalStore from "@/store/modal";
+import sfx from "@/assets/sound/sfx_sounds_Blip7.wav";
+import bgm from "@/assets/sound/bgm_25_Route_30.mp3";
 
 function Gameboy() {
-  // const { pushModals, popModals } = modalStore();
+  const sfxRef = useRef<HTMLAudioElement>(null);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
-  // const [value, setValue] = useState("");
+  const [typingStart, setTypingStart] = useState(false);
+  const [sound, setSound] = useState(true);
   const [loaded, setLoaded] = useState(false);
+
+  const handleBgmPlay = (onOff: boolean) => {
+    if (audioRef.current && onOff === true) {
+      audioRef.current.play();
+    } else if (audioRef.current && onOff === false) {
+      audioRef.current.pause();
+    }
+  };
+
+  const handleSfxPlay = () => {
+    if (sfxRef.current) {
+      sfxRef.current.play();
+    }
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -18,13 +34,39 @@ function Gameboy() {
     }, 1000);
   }, []);
 
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (sound) {
+      handleBgmPlay(true);
+
+      if (typingStart) {
+        timer = setInterval(() => {
+          handleSfxPlay();
+        }, 40);
+      }
+    } else {
+      handleBgmPlay(false);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [sound, typingStart]);
+
   return (
     <div className="gameboy">
       <div className="gameboy-screen-cont">
         <div className="gameboy-screen">
           <div className="power power-on"></div>
-          <label id="mute-toggle" className="sound sound-off">
-            <input id="mute" type="checkbox" style={{ display: "none" }} />
+          <label id="mute-toggle" className={`sound ${sound ? "sound-on" : "sound-off"}`}>
+            <input
+              id="mute"
+              type="checkbox"
+              style={{ display: "none" }}
+              onClick={() => setSound((prev) => !prev)}
+            />
+            <audio className="hidden" ref={audioRef} src={bgm} loop />
           </label>
           <div className="header">DOT MATRIX WITH STEREO SOUND</div>
           <div className="main">
@@ -33,10 +75,37 @@ function Gameboy() {
                 <div className="ash">
                   <img src={ash.src} alt="ash" />
                 </div>
-                <span className="text" style={{ whiteSpace: "break-spaces" }}></span>
+                <div className="text" style={{ whiteSpace: "break-spaces" }}>
+                  <div>
+                    <audio className="hidden" ref={sfxRef} src={sfx} />
+                    <ReactTyped
+                      strings={[
+                        'NickName : 임대현^1000\n\nBirth : 1991 / 12 / 30^1000\n\nGender : 남자^1000\n\nJob : 모험을 좋아하는 방구석 웹 개발자..^1000 `\n\n\n<div class="close"><a href="/"" >[ Back ]</a></div>\n\n`',
+                      ]}
+                      startDelay={2000}
+                      backSpeed={100}
+                      typeSpeed={100}
+                      preStringTyped={() => {
+                        setTypingStart(true);
+                      }}
+                      onTypingPaused={() => {
+                        setTypingStart(false);
+                      }}
+                      onTypingResumed={() => {
+                        setTypingStart(true);
+                      }}
+                      onStringTyped={() => {
+                        setTypingStart(false);
+                      }}
+                      onComplete={() => {
+                        setTypingStart(false);
+                      }}
+                    />
+                  </div>
+                </div>
               </>
             ) : (
-              <div className="flex w-full h-[calc(100vh-6rem)] items-center justify-center">
+              <div className="loading">
                 <img src={loading.src} alt="loading" className="object-cover w-[250px] h-[85px]" />
               </div>
             )}
