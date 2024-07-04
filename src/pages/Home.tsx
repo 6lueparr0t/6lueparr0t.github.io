899c9539d00dbca0266056b558236000a16767d9
6lueparr0t <6lueparr0t@gmail.com>
2024-07-05 03:39:59 +0900

    feat: main 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div className="font-['DungGeunMo'] mx-auto p-4 w-[768px] max-w-[calc(100%-2rem)] text-center">
     <h1 className="text-6xl break-keep m-4 text-gradient">Hello, World!</h1>
-    <h2 className="text-4xl break-keep text-gradient">개발자 발가락</h2>
+    <h2 className="text-4xl break-keep text-gradient">One For Line, Line For All</h2>
   </div>
   );
 }
