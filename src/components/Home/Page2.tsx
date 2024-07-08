fce7dc3245b5e8eb0717a8466687c0875a3c3e18
6lueparr0t <6lueparr0t@gmail.com>
2024-07-09 01:14:02 +0900

    misc

--- src/components/Home/Page2.tsx
+++ src/components/Home/Page2.tsx
@@ -4,7 +4,7 @@ function Page2() {
   return (
     <div className="mx-auto text-center max-w-[calc(100%)] h-[calc(100vh-12rem)] bg-slate-100 dark:bg-slate-900">
       <div className="flex sticky pt-4 top-0">
-        <img className="w-[64px] h-[64px]" src={ash2} alt="ash-going" />
+        <img className="w-[20px] h-[20px]" src={ash2} alt="ash-going" />
       </div>
     </div>
   );
--- src/components/_Common/Header.tsx
+++ src/components/_Common/Header.tsx
@@ -1,6 +1,5 @@
 import React, { type PropsWithChildren } from "react";
 import { NavLink } from "react-router-dom";
-import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
 
 const MENU: { path: string; title: string }[] = [
@@ -22,8 +21,8 @@ const Header: React.FC<PropsWithChildren> = ({ children }) => {
           {MENU.map((menu) => {
             return (
               <li key={menu.path}>
-                <NavLink to={menu.path}>
-                  <Button>{menu.title}</Button>
+                <NavLink to={menu.path} className={"text-gray-800 hover:text-gray-400  dark:text-gray-100 dark:hover:text-gray-500"} end>
+                  {menu.title}
                 </NavLink>
               </li>
             );
