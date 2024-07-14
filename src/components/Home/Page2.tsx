7dd0fc763591e2b2af5ec12aed22bef75cd76e6b
6lueparr0t <6lueparr0t@gmail.com>
2024-07-14 18:56:54 +0900

    fix: 메뉴 안눌리는 버그 수정

--- src/components/Home/Page2.tsx
+++ src/components/Home/Page2.tsx
@@ -2,8 +2,8 @@ import Ash from "./Ash";
 
 function Page2() {
   return (
-    <div className="mx-auto text-center max-w-[calc(100%)] h-[calc(100lvh)] bg-slate-100 dark:bg-slate-900">
-      <div className="flex sticky justify-center top-[0.4rem] py-4 z-10">Experience</div>
+    <div className="mx-auto text-center h-[calc(100lvh)] bg-slate-100 dark:bg-slate-900">
+      <div className="inline-block sticky justify-center top-[0.4rem] py-4 z-10">Experience</div>
       <div className="flex items-end h-[calc(100lvh-5rem)] p-4">
         <Ash />
       </div>
