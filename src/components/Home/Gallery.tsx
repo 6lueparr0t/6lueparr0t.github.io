86614de8864c2e2b38ea4766a539be8a558b9298
6lueparr0t <6lueparr0t@gmail.com>
2024-07-19 06:30:25 +0900

    feat: gallery 에 키보드 이벤트 추가

--- src/components/Home/Gallery.tsx
+++ src/components/Home/Gallery.tsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import {
   type CarouselApi,
   Carousel,
   CarouselContent,
   CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
 } from "@/components/ui/carousel";
 // import Autoplay from "embla-carousel-autoplay";
 
@@ -12,16 +14,32 @@ import profile2 from "@/assets/home/profile2.webp";
 import profile3 from "@/assets/home/profile3.webp";
 import profile4 from "@/assets/home/profile4.webp";
 
-const IMAGES:string[] = [
-  profile1,
-  profile2,
-  profile3,
-  profile4,
-]
+const IMAGES: string[] = [profile1, profile2, profile3, profile4];
 
 export const Gallery: React.FC = () => {
+  const componentRef = useRef<HTMLDivElement>(null);
   const [api, setApi] = useState<CarouselApi>();
   const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting && componentRef.current) {
+        componentRef.current.focus({ preventScroll: true });
+        console.log("Gallery is in view");
+      }
+    });
+
+    if (componentRef.current) {
+      observer.observe(componentRef.current);
+    }
+
+    return () => {
+      if (componentRef.current) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        observer.unobserve(componentRef.current);
+      }
+    };
+  }, []);
 
   useEffect(() => {
     if (!api) return;
@@ -34,22 +52,34 @@ export const Gallery: React.FC = () => {
 
   return (
     <Carousel
+      className="outline-none"
+      ref={componentRef}
       setApi={setApi}
       opts={{
         loop: true,
       }}
-      plugins={[
-        // Autoplay({
-        //   delay: 5000,
-        //   stopOnInteraction: false,
-        //   stopOnMouseEnter: true,
-        // }),
-      ]}
+      plugins={
+        [
+          // Autoplay({
+          //   delay: 5000,
+          //   stopOnInteraction: false,
+          //   stopOnMouseEnter: true,
+          // }),
+        ]
+      }
+      onKeyDownCapture={(e) => {
+        if (api && e.key === "ArrowRight") {
+          api.scrollNext();
+        } else if (api && e.key === "ArrowLeft") {
+          api.scrollPrev();
+        }
+      }}
+      tabIndex={0}
     >
       <CarouselContent>
         {IMAGES.map((image) => (
           <CarouselItem key={image} className="flex items-center justify-center">
-            <img className="flex items-center" src={image} alt={`Placeholder Image ${image}`}/>
+            <img className="flex items-center" src={image} alt={`Placeholder Image ${image}`} />
           </CarouselItem>
         ))}
       </CarouselContent>
@@ -64,6 +94,8 @@ export const Gallery: React.FC = () => {
           />
         ))}
       </div>
+      <CarouselPrevious />
+      <CarouselNext />
     </Carousel>
   );
 };
