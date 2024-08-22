4c05ca2cd842ec38afe251f2f1fdf9203801ab36
6lueparr0t <6lueparr0t@gmail.com>
2024-08-22 23:17:56 +0900

    feat: turnstile 위치 조절2

--- src/components/guest/GuestbookForm.tsx
+++ src/components/guest/GuestbookForm.tsx
@@ -101,7 +101,7 @@ const GuestbookForm = () => {
             className="mt-1 block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm text-black dark:text-gray-200 bg-white dark:bg-gray-900 focus:outline-none focus:ring-slate-500 focus:border-slate-500 sm:text-sm"
           />
         </div>
-        <div className='flex justify-center mt-4'>
+        <div className='flex justify-center mt-6'>
           <Turnstile
             ref={turnstileRef}
             siteKey='0x4AAAAAAAh0AbQiSvRJjo-z'
