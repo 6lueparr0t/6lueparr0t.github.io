4441b0af003c57e51b78014565913c13025ca0ce
6lueparr0t <6lueparr0t@gmail.com>
2024-08-26 00:30:59 +0900

    feat: guestbook endpoint .env 로2

--- src/components/guest/GuestbookForm.tsx
+++ src/components/guest/GuestbookForm.tsx
@@ -39,7 +39,7 @@ const GuestbookForm = () => {
     setSuccess(null);
 
     try {
-      const response = await fetch(import.meta.env.VITE_APP_SLACK_ENDPOINT, {
+      const response = await fetch(`${import.meta.env.VITE_APP_SLACK_ENDPOINT}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
