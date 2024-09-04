f7fe59776dbd6cb291a98c9d654c0e52fe3ba38e
6lueparr0t <6lueparr0t@gmail.com>
2024-09-05 00:33:35 +0900

    perf: 코드 개선

--- src/components/home/Page3.tsx
+++ src/components/home/Page3.tsx
@@ -7,6 +7,7 @@ import { Job } from "@/components/components.d";
 function Page3() {
   return (
     <>
+      {/* Experiences Section */}
       <div id="experiences" className="mx-auto text-center bg-zinc-50 dark:bg-zinc-950">
         <div className="text-xl md:text-2xl top-[0.4rem] md:top-[0.2rem] inline-block sticky justify-center py-4 z-10">
           Experiences
@@ -20,35 +21,24 @@ function Page3() {
           </div>
         </div>
       </div>
+
+      {/* Certifications Section */}
       <div className="mb-16 flex flex-col items-center space-y-2">
         <h2 className="text-2xl mb-4">Certifications</h2>
-        {/* // ? INFO 자격증 개수 변경 시, tailwind.config.js -> scroll animation 조절 필요 (현재: 4) */}
+        {/* 자격증 스크롤 애니메이션 */}
         <div className="relative w-[236px * 3 * 4]">
           <div className="flex space-x-4 justify-center animate-scroll hover:[animation-play-state:paused]">
-            {certifications.map((certification) => (
-              <CertificationCard
-                key={certification.title}
-                title={certification.title}
-                issuer={certification.issuer}
-                date={certification.date}
-              />
-            ))}
-            {certifications.map((certification) => (
-              <CertificationCard
-                key={certification.title + "2"}
-                title={certification.title}
-                issuer={certification.issuer}
-                date={certification.date}
-              />
-            ))}
-            {certifications.map((certification) => (
-              <CertificationCard
-                key={certification.title + "3"}
-                title={certification.title}
-                issuer={certification.issuer}
-                date={certification.date}
-              />
-            ))}
+            {Array(5)
+              .fill(certifications)
+              .flat()
+              .map((certification, index) => (
+                <CertificationCard
+                  key={`${certification.title}-${index}`}
+                  title={certification.title}
+                  issuer={certification.issuer}
+                  date={certification.date}
+                />
+              ))}
           </div>
         </div>
       </div>
