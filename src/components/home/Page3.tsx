c9b0acc8a0e3376b0c4740c78cfbc4388dabd0f1
6lueparr0t <6lueparr0t@gmail.com>
2024-09-04 09:37:17 +0900

    feat: 자격증란 추가

--- src/components/home/Page3.tsx
+++ src/components/home/Page3.tsx
@@ -1,18 +1,19 @@
-import experience from "./experience.json";
 import ExperienceList from "./ExperienceList";
+import experiences from "./Experiences.json";
+import certifications from "./Certifications.json";
 import { Job } from "@/components/components.d";
 
 function Page3() {
   return (
     <>
-      <div id="experience" className="mx-auto text-center bg-zinc-50 dark:bg-zinc-950">
+      <div id="experiences" className="mx-auto text-center bg-zinc-50 dark:bg-zinc-950">
         <div className="text-xl md:text-2xl top-[0.4rem] md:top-[0.2rem] inline-block sticky justify-center py-4 z-10">
-          experience
+          Experiences
         </div>
 
         <div className="flex flex-col justify-center items-center w-full">
           <div className="font-noto w-full sm:w-full md:w-full lg:w-2/3 xl:w-1/2 text-left break-keep">
-            {experience.map((job) => (
+            {experiences.map((job) => (
               <ExperienceList key={job.title} job={job as Job} />
             ))}
           </div>
@@ -21,18 +22,14 @@ function Page3() {
       <div className="mb-16 flex flex-col items-center space-y-2">
         <h2 className="text-2xl">Certifications</h2>
         <div className="flex flex-col items-center font-noto">
-          <div className="text-xl">
-            정보처리기사 <span className="text-sm">(한국산업인력공단, 2019.08)</span>
-          </div>
-          <div className="text-xl">
-            컴퓨터활용능력 1급 <span className="text-sm">(대한상공회의소, 2009.08)</span>
-          </div>
-          <div className="text-xl">
-            CKAD <span className="text-sm">(The Linux Foundation, 2023.02)</span>
-          </div>
-          <div className="text-xl">
-            AWS Developer Associate <span className="text-sm">(Amazon Web Services, 2022.12)</span>
-          </div>
+          {certifications.map((certification) => (
+            <div key={certification.title} className="text-xl">
+              {certification.title}{" "}
+              <span className="text-sm">
+                ({certification.issuer}, {certification.date})
+              </span>
+            </div>
+          ))}
         </div>
       </div>
     </>
