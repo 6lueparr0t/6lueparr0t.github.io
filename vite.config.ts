dbc17af34a2eb16b19f99124dcff2bf31635698a
6lueparr0t <6lueparr0t@gmail.com>
2025-01-16 22:27:04 +0900

    feat: vitest 추가 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { configDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), basicSsl()],
-  test: {
-    environment: "jsdom",
-    globals: true, // global 변수 활성화 (e.g., describe, test 등)
-    setupFiles: "./vitest.setup.ts", // 추가 설정 파일
-    css: true, // CSS 속성 테스트 활성화
-    exclude: [...configDefaults.exclude, "node_modules/**"], // 기본 제외 경로 포함
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import path from "path";
+import { defineConfig } from "vitest/config";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+    globals: true, // global 변수 활성화 (e.g., describe, test 등)
+    setupFiles: "./vitest.setup.ts", // 추가 설정 파일
+    css: true, // CSS 속성 테스트 활성화
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
